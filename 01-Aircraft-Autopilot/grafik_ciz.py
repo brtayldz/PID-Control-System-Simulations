c1b73c5e237411024f35a920fbc8a756051156ae
brtayldz <87896798+brtayldz@users.noreply.github.com>
2026-06-21 01:50:28 +0300

    Refactor grafik_ciz.py for clarity and structure

--- 01-Aircraft-Autopilot/grafik_ciz.py
+++ 01-Aircraft-Autopilot/grafik_ciz.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 saniyeler = []
 yukseklikler = []
 
-# C kodunun ürettiği dosyayı okuyoruz
 try:
     with open("simulasyon_veri.txt", "r") as dosya:
         for satir in dosya:
@@ -14,17 +13,15 @@ except FileNotFoundError:
     print("Hata: 'simulasyon_veri.txt' bulunamadı! Önce C kodunu çalıştır.")
     exit()
 
-# Grafiği janti bir mühendislik raporu moduna sokuyoruz
+
 plt.figure(figsize=(10, 5))
 plt.plot(saniyeler, yukseklikler, label="Uçağın Yüksekliği", color="red", linewidth=2)
-plt.axhline(y=50, color="blue", linestyle="--", label="Hedef (50m)") # Hedef çizgisi
+plt.axhline(y=50, color="blue", linestyle="--", label="Hedef (50m)")
 
-# Grafik süslemeleri
 plt.title("PID Otopilot Yükseklik Simülasyonu", fontsize=14, fontweight='bold')
 plt.xlabel("Zaman (Saniye)", fontsize=12)
 plt.ylabel("Yükseklik (Metre)", fontsize=12)
 plt.grid(True, linestyle=":", alpha=0.6)
 plt.legend(loc="lower right")
 
-# Grafiği ekrana basıyoruz
 plt.show()
